0f2f316923f2a18f3b44061fc6b3626e158995a0
NOFACE_NOMAD <shaikielyn16@gmail.com>
2026-08-29 17:10:03 +0300

    feat: load bright project page styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./bright.css";
+import "./bright-pages.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
